8a102839510d7b939188c0e4d07c2668da9f3f8f
dillenger <dillinger@kpnmail.nl>
2022-10-13 11:06:09 +0200

    updates

---
+++ background.js
@@ -0,0 +1,48 @@
+messenger.WindowListener.registerChromeUrl([
+    ["content",  "PhoenityIcons", "content/"]
+]);
+
+
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/addressbook/addressbook.xhtml",
+    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
+
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/addressbook/abContactsPanel.xhtml",
+    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
+
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/messengercompose/messengercompose.xhtml",
+    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
+
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/messenger.xhtml",
+    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
+
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/messageWindow.xhtml",
+    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
+
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/customizeToolbar.xhtml",
+    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
+
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/activity.xhtml",
+    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
+
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/subscribe.xhtml",
+    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
+
+messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/SearchDialog.xhtml",
+    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
+
+messenger.WindowListener.startListening();
+
+async function loadIconImages() {
+  await messenger.phoenityIconsApi.phoenityIcons();
+}
+
+loadIconImages();
---
+++ content/scripts/loadMessenger.js
@@ -0,0 +1,52 @@
+var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+const fileName = "messenger.css";
+
+function onLoad(activatedWhileWindowOpen) {
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/" + fileName);
+
+  if (Services.appinfo.OS == "WINNT") {
+    WL.injectCSS("chrome://PhoenityIcons/content/aero/" + fileName);
+  } else if (Services.appinfo.OS == "Linux") {
+    WL.injectCSS("chrome://PhoenityIcons/content/linux/" + fileName);
+  } else if (Services.appinfo.OS == "Darwin") {
+    WL.injectCSS("chrome://PhoenityIcons/content/darwin/" + fileName);
+  }
+
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/primaryToolbar.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/folderPane.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/folderMenus.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/chat.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/calendar/calendar.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/messageIcons.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/quickFilterBar.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/mailWindow1.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/activity/activity.css");
+
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/messengercompose/messengercompose.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/addressbook/addressbook.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/addressbook/abContactsPanel.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/addressbook/abResultsPane.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/addressbook/cardDialog.css");
+
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/uBlockOrigin/uBlockOrigin.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/cardbook/cardbook.css");
+  WL.injectCSS("chrome://PhoenityIcons/content/skin/mailredirect/mailredirect.css");
+
+  window.addEventListener("MsgsLoaded", msgListener);
+}
+
+function onUnload(deactivatedWhileWindowOpen) {
+  window.removeEventListener("MsgsLoaded", msgListener);
+}
+
+function msgListener () {
+  let multimessage = window.document.getElementById("multimessage");
+  if (multimessage) {
+    let w = multimessage.contentWindow.wrappedJSObject;
+    linkElement = w.document.createElement("link");
+    linkElement.setAttribute("rel", "stylesheet");
+    linkElement.setAttribute("href", "chrome://PhoenityIcons/content/skin/primaryToolbar.css");
+    w.document.documentElement.appendChild(linkElement);
+  }
+}
