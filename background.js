b24d00323d39267b13a094cf05ca170471eaf802
dillenger <dillinger@kpnmail.nl>
2023-05-19 06:49:22 +0200

    Supernova update

--- background.js
+++ background.js
@@ -1,38 +1,32 @@
-messenger.WindowListener.registerChromeUrl([
-    ["content",  "PhoenityIcons", "content/"]
-]);
-
-messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/messengercompose/messengercompose.xhtml",
-    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
-
-messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/messenger.xhtml",
-    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
-
-messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/messageWindow.xhtml",
-    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
-
-messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/customizeToolbar.xhtml",
-    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
-
-messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/SearchDialog.xhtml",
-    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
-
-messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/about3Pane.xhtml",
-    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
-
-messenger.WindowListener.registerWindow(
-    "chrome://messenger/content/aboutMessage.xhtml",
-    "chrome://PhoenityIcons/content/scripts/loadMessenger.js");
-
-messenger.WindowListener.startListening();
-
 async function loadIconImages() {
+
+  messenger.WindowListener.registerChromeUrl([
+    ["content",  "PhoenityIcons", "content/"]
+  ]);
+
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/messenger.xhtml", "content/scripts/loadMessenger.js");
+
+  messenger.WindowListener.registerWindow(
+    "about:3pane", "content/scripts/loadMessenger.js");
+
+  messenger.WindowListener.registerWindow(
+    "about:message", "content/scripts/loadMessenger.js");
+
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/messageWindow.xhtml", "content/scripts/loadMessenger.js");
+
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/messengercompose/messengercompose.xhtml", "content/scripts/loadMessenger.js");
+
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/customizeToolbar.xhtml", "content/scripts/loadMessenger.js");
+
+  messenger.WindowListener.registerWindow(
+    "chrome://messenger/content/SearchDialog.xhtml", "content/scripts/loadMessenger.js");
+
+  messenger.WindowListener.startListening();
+
   await messenger.phoenityIconsApi.phoenityIcons();
 }
 
